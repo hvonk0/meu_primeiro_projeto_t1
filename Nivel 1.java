205b037aa9f3e5ba43dde6c2ec44110ef72f4b5d
Hary Vonk <harycosta0@gmail.com>
2026-08-28 16:25:03 -0300

    Add files via upload

---
+++ Nivel 1.java
@@ -0,0 +1,8 @@
+public class Nivel1Ex1 {
+    public static void main(String[] args) {
+        int numeroInt = 42;
+        double numeroDouble = numeroInt; // Casting implícito
+
+        System.out.println("Valor em double: " + numeroDouble);
+    }
+}
---
+++ Nivel 2.java
@@ -0,0 +1,9 @@
+public class Nivel1Ex2 {
+    public static void main(String[] args) {
+        double valorDouble = 9.99;
+        int valorInt = (int) valorDouble; // Casting explícito
+        
+        System.out.println("Valor Double original: " + valorDouble);
+        System.out.println("Valor Int após casting: " + valorInt);
+    }
+}
